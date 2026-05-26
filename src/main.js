6e7ece2757b4866d1a6e00f5fbee14c7cb9dad07
Anatolii Lokatyr <andyone4311@gmail.com>
2026-05-26 12:52:53 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+console.log("Webflow script loaded");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
+});
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/main.js",
+      name: "WebflowScripts",
+      formats: ["iife"],
+      fileName: () => "index.js",
+    },
+  },
+});
