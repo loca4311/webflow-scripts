55333a430ad9c5f9959d19ecc73a5b16198cfb69
Anatolii Lokatyr <andyone4311@gmail.com>
2026-07-08 12:45:35 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   const form = document.querySelector("#orderForm");
   if (!form) return;
 
-  console.log("TEST DEPLOY 123");
-
   const CHECK_MEMBER_EMAIL_ENDPOINT =
     "https://tinguvlwumswhznygirl.supabase.co/functions/v1/check-member-email";
 
